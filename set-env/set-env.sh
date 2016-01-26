0236ae61a065d771352e3c05b32babdce952b4ae
clutcher <iclutcher@gmail.com>
2016-01-26 16:48:20 +0200

    Java and xkb fixes. Gnome-terminal settings, mc settings.

--- set-env/set-env.sh
+++ set-env/set-env.sh
@@ -2,9 +2,10 @@
 #Set environment
 
 sudo apt-get install aptitude
-sudo aptitude install sakura volti imagemagick
+sudo aptitude install gnome-terminal volti imagemagick
 sudo aptitude install git mercurial
 sudo aptitude install libglib2.0-bin
+sudo aptitude install dconf-editor
 
 cp -r gtk ~/.themes
 
@@ -23,8 +24,7 @@ cat <<EOT > ~/.config/gtk-3.0/settings.ini
 gtk-application-prefer-dark-theme=1
 EOT
 
-######## Sakura term settings ##########
-cp -r sakura ~/.config
+######## Gnome terminal settings ##########
 
 
 ######## Sublime install ########
@@ -34,3 +34,23 @@ echo "################"
 echo "Please edit /etc/apt/sources.list.d/ to get it work properly"
 
 cp -r sublime-text-3 ~/.config
+
+######## Fix xkb keyboard bug #########
+xkbcomp $DISPLAY - | egrep -v 'group . = AltGr;' | xkbcomp - $DISPLAY
+
+######## Fix Java bugs ##########
+IRONIC_WM_NAME="Sawfish"
+NET_WIN=$(xprop -root _NET_SUPPORTING_WM_CHECK | awk -F "# " '{print $2}')
+
+if [[ "$NET_WIN" == 0x* ]]; then
+    # xprop cannot reliably set UTF8_STRING, so we replace as string.
+    # fortunately, jdk is OK with this, but wm-spec says use UTF8_STRING.
+    xprop -id "$NET_WIN" -remove _NET_WM_NAME
+    xprop -id "$NET_WIN" -f _NET_WM_NAME 8s -set _NET_WM_NAME "$IRONIC_WM_NAME"
+else
+    # even if we're not net compatible, do java workaround
+    xprop -root -remove _NET_WM_NAME
+    xprop -root -f _NET_WM_NAME 8s -set _NET_WM_NAME "$IRONIC_WM_NAME"
+fi
+
+
