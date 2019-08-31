14c4e31c64b1b1cd0edb9b739528848f6ce89fe2
Igor Zarvanskyi <iclutcher@gmail.com>
2019-08-31 08:06:39 +0300

    Add nautilus folder setup

--- scripts/configure_global_settings.sh
+++ scripts/configure_global_settings.sh
@@ -6,6 +6,13 @@ cat <<EOT > ~/.config/gtk-3.0/settings.ini
 gtk-application-prefer-dark-theme=1
 EOT
 
+# Nautilus folders settings
+cat <<EOT > ~/.config/user-dirs.dirs
+XDG_DOWNLOAD_DIR="$HOME/Downloads"
+XDG_DOCUMENTS_DIR="$HOME/projects"
+XDG_PICTURES_DIR="$HOME/screenshots"
+EOT
+
 # Git settings
 git config --global user.name "Igor Zarvanskyi"
 git config --global user.email "iclutcher@gmail.com"
