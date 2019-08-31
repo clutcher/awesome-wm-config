09dce5f73f06727bf33473a0b4707cc6372372c2
Igor Zarvanskyi <iclutcher@gmail.com>
2019-08-31 07:57:19 +0300

    Failsafe symlink creation for IDEA install

--- scripts/install_intellij.sh
+++ scripts/install_intellij.sh
@@ -2,4 +2,4 @@
 
 sudo mkdir -p /opt/idea
 sudo curl -L "https://download.jetbrains.com/product?code=IIU&latest&distribution=linux" | sudo tar xz -C /opt/idea --strip 1
-sudo ln -s /opt/idea/bin/idea.sh /usr/local/bin/idea
+sudo ln -sf /opt/idea/bin/idea.sh /usr/local/bin/idea
