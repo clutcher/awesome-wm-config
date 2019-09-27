d7080b92bd77d177ca071f9b34945a1aed10da56
Igor Zarvanskyi <iclutcher@gmail.com>
2019-09-27 11:52:31 +0300

    Change rights of idea to be able to update idea from current user

--- scripts/install_intellij.sh
+++ scripts/install_intellij.sh
@@ -2,4 +2,11 @@
 
 sudo mkdir -p /opt/idea
 sudo curl -L "https://download.jetbrains.com/product?code=IIU&latest&distribution=linux" | sudo tar xz -C /opt/idea --strip 1
+sudo chown -R  "$(id -u):$(id -g)" /opt/idea
 sudo ln -sf /opt/idea/bin/idea.sh /usr/local/bin/idea
+
+
+# sudo mkdir -p /opt/pycharm
+# sudo curl -L "https://download.jetbrains.com/product?code=PCP&latest&distribution=linux" | sudo tar xz -C /opt/pycharm --strip 1
+# sudo chown -R  $(id -u):$(id -g) /opt/pycharm
+# sudo ln -sf /opt/pycharm/bin/pycharm.sh /usr/local/bin/pycharm
