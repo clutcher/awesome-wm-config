69bbcc7db83e492035bd7def420753a0144712e0
Igor Zarvanskyi <iclutcher@gmail.com>
2022-12-23 13:53:40 +0100

    Fix docker repository gpg keys. Disable docker service by default.

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -5,7 +5,9 @@ sudo apt remove docker docker-engine docker.io containerd runc
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo mkdir -p /etc/apt/trusted.gpg.d/
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
+
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian buster stable"
 
 sudo apt update
@@ -14,3 +16,8 @@ sudo apt install docker-ce docker-ce-cli containerd.io
 # Add permission for current user
 sudo usermod -aG docker $USER
 sudo chmod 666 /var/run/docker.sock
+
+# Disable docker service on startup. Would be enabled manually only when needed.
+sudo systemctl disable docker.service
+sudo systemctl disable containerd.service
+sudo systemctl disable ModemManager.service
