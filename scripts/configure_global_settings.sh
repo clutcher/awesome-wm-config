112fb06f4a8e728d7ce829fefe75077c9cb1dcf2
Igor Zarvanskyi <iclutcher@gmail.com>
2022-12-23 13:55:13 +0100

    Update global settings

--- scripts/configure_global_settings.sh
+++ scripts/configure_global_settings.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# GNOME settings
-cat <<EOT > ~/.config/gtk-3.0/settings.ini
-[Settings]
-gtk-application-prefer-dark-theme=1
-EOT
-
 # System folders settings
 cat <<EOT > ~/.config/user-dirs.dirs
 XDG_DOWNLOAD_DIR="$HOME/Downloads"
@@ -13,6 +7,9 @@ XDG_DOCUMENTS_DIR="$HOME/projects"
 XDG_PICTURES_DIR="$HOME/screenshots"
 EOT
 
-# Git settings
+# Generate new ssh keys
+ssh-keygen -t rsa -C "iclutcher@gmail.com" -f ~/.ssh/id_rsa -q -N ""
+
+# Git 9settings
 git config --global user.name "Igor Zarvanskyi"
 git config --global user.email "iclutcher@gmail.com"
