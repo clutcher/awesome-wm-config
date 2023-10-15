4a48e1e41f9ec04d9a62a2b854b6b29e92223878
Igor Zarvanskyi <iclutcher@gmail.com>
2023-10-15 20:51:59 +0200

    Add emoji font and autodiscovery of Windows in GRUB

--- scripts/setup_hardware_and_cli.sh
+++ scripts/setup_hardware_and_cli.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
 # Install cli packages for comfort work
-sudo apt install mc jq zip\
- curl wget \
- git git-lfs\
- imagemagick openvpn
+sudo apt install mc jq zip curl wget \
+    git git-lfs imagemagick openvpn \
+    net-tools \
+    fonts-noto-color-emoji
+
+sudo fc-cache -fv
 
 # Install libraries
 sudo apt install libglib2.0-bin
@@ -12,3 +16,7 @@ sudo apt install firmware-linux
 
 # Hardware support
 sudo apt install network-manager bluez pipewire-audio alsa-utils
+
+# Enable os prober to add Windows into grub
+sudo sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
+sudo update-grub
