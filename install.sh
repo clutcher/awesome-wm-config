a1cf93951bae7ed376059f5aed2608eb2d9c069d
Igor Zarvanskyi <iclutcher@gmail.com>
2023-10-15 13:07:25 +0200

    Add Obsidian installation

--- install.sh
+++ install.sh
@@ -3,22 +3,24 @@
 sudo apt update
 
 source scripts/setup_hardware_and_cli.sh
-source scripts/setup_awesome.sh
-source scripts/setup_xfce_de.sh
-source scripts/configure_global_settings.sh
 source scripts/configure_backlight.sh
 ## source scripts/configure_touchpad.sh
 
+source scripts/configure_global_settings.sh
+
+source scripts/setup_awesome.sh
+source scripts/setup_xfce_de.sh
 source scripts/install_chrome.sh
 
-source scripts/install_intellij.sh
+# Copy and replace dotfiles
+cp -rf dotfiles/* ~/.config/
 
 source scripts/install_java.sh
 source scripts/install_nodejs.sh
 
-# ADD install for obsidian
-source scripts/install_docker.sh
-source scripts/install_insync.sh
+source scripts/install_intellij.sh
 
-# Copy and replace dotfiles
-cp -rf dotfiles/* ~/.config/
+source scripts/install_docker.sh
+
+source scripts/install_obsidian.sh
+source scripts/install_insync.sh
---
+++ scripts/install_obsidian.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# Install latest obsidian
+VER=$(curl --silent -qI https://github.com/obsidianmd/obsidian-releases/releases/latest | awk -F '/' '/^location/ {print  substr($NF, 1, length($NF)-1)}'); \
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/$VER/obsidian_${VER#v}_amd64.deb -O /tmp/obsidian.deb
+
+sudo dpkg -i /tmp/obsidian.deb
+
+# Clone PKM
+mkdir -p ~/insync/knowledge
+git clone https://github.com/clutcher/knowledge.git ~/insync/knowledge/
