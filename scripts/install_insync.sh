a1c7f3d9f715641ecdbb77fc81080069e748d56d
Igor Zarvanskyi <iclutcher@gmail.com>
2023-10-15 13:04:54 +0200

    Update insync repository

--- scripts/install_insync.sh
+++ scripts/install_insync.sh
@@ -2,6 +2,6 @@
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C
 
-echo "deb http://apt.insync.io/ubuntu kinetic non-free contrib" | sudo tee /etc/apt/sources.list.d/insync.list
+echo "deb http://apt.insync.io/ubuntu jammy non-free contrib" | sudo tee /etc/apt/sources.list.d/insync.list
 sudo apt update
 sudo apt install insync
