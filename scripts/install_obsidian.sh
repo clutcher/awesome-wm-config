037b99f9d7ed0c7f2420d8ec0441ee2d4bb2c79d
Igor Zarvanskyi <iclutcher@gmail.com>
2023-10-16 06:06:02 +0200

    Disable cloning of knowledge DB

--- scripts/install_obsidian.sh
+++ scripts/install_obsidian.sh
@@ -7,5 +7,5 @@ wget https://github.com/obsidianmd/obsidian-releases/releases/download/$VER/obsi
 sudo dpkg -i /tmp/obsidian.deb
 
 # Clone PKM
-mkdir -p ~/insync/knowledge
-git clone https://github.com/clutcher/knowledge.git ~/insync/knowledge/
+#mkdir -p ~/insync/knowledge
+#git clone https://github.com/clutcher/knowledge.git ~/insync/knowledge/
