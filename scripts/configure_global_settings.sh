7cb69ecb11d1027be1352c110bfe2436f2e9c6ee
Igor Zarvanskyi <iclutcher@gmail.com>
2023-10-16 07:33:17 +0200

    Add git credential caching

--- scripts/configure_global_settings.sh
+++ scripts/configure_global_settings.sh
@@ -13,3 +13,4 @@ ssh-keygen -t rsa -C "iclutcher@gmail.com" -f ~/.ssh/id_rsa -q -N ""
 # Git settings
 git config --global user.name "Igor Zarvanskyi"
 git config --global user.email "iclutcher@gmail.com"
+git config credential.helper 'cache --timeout=3600'
