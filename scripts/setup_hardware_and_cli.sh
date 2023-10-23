577339939d17349ced7af6db7a0e4465e853343d
Igor Zarvanskyi <iclutcher@gmail.com>
2023-10-23 08:53:54 +0200

    Add udev rule for mechanical keyboard

--- scripts/setup_hardware_and_cli.sh
+++ scripts/setup_hardware_and_cli.sh
@@ -17,6 +17,11 @@ sudo apt install firmware-linux
 # Hardware support
 sudo apt install network-manager bluez pipewire-audio alsa-utils
 
+# Setup udev rules
+sudo cp -rf configs/udev/* /etc/udev/rules.d/
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+
 # Enable os prober to add Windows into grub
 sudo sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
 sudo update-grub
