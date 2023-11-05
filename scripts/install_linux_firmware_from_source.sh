b2245e2c87a65a6b0ecb449115682003e0d7c883
Igor Zarvanskyi <iclutcher@gmail.com>
2023-11-05 19:05:41 +0100

    Add installation of latest linux firmware from source
       Can be useful for latest hardware. Otherwise, use
       firmware from debian repository.

---
+++ scripts/install_linux_firmware_from_source.sh
@@ -0,0 +1,8 @@
+FIRMWARE_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/
+latest_version=$(git ls-remote --tags $FIRMWARE_REPO | sort -t '/' -k 3 -V | awk -F/ '{ print $3 }' | awk '!/\^\{\}/' | tail -n 1)
+
+rm -rf /tmp/firmware/
+mkdir -p /tmp/firmware
+git clone --branch $latest_version --depth 1 $FIRMWARE_REPO /tmp/firmware
+
+sudo cp -rf /tmp/firmware/* /lib/firmware
