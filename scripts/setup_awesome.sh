239ddc8150487ad2d0c1af7771faa5ba02bb335a
Igor Zarvanskyi <iclutcher@gmail.com>
2023-11-05 18:38:29 +0100

    Minor rearrange of packages

--- scripts/setup_awesome.sh
+++ scripts/setup_awesome.sh
@@ -4,14 +4,14 @@
 sudo apt install lightdm
 
 # Install awesomewm
-sudo apt install awesome awesome-extra
+sudo apt install awesome awesome-extra picom
 #sudo apt purge gnome* gdm3
 
 # Additional tools used by copycats theme
 sudo apt install unclutter xsel xfonts-terminus
 
 # Install application runner with themes
-sudo apt install rofi picom fonts-roboto
+sudo apt install rofi fonts-roboto
 
 mkdir -p ~/.local/share/rofi/themes/
 curl -o ~/.local/share/rofi/themes/rounded-common.rasi https://raw.githubusercontent.com/newmanls/rofi-themes-collection/master/themes/rounded-common.rasi
