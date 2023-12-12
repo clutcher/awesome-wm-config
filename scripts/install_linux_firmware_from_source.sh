7be34b7f4bdaf9f27bbcb0228fdc9d176a63bf0f
Igor Zarvanskyi <iclutcher@gmail.com>
2023-12-12 18:23:57 +0100

    Update initramfs after firmware replace

--- scripts/install_linux_firmware_from_source.sh
+++ scripts/install_linux_firmware_from_source.sh
@@ -1,8 +1,13 @@
 FIRMWARE_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/
 latest_version=$(git ls-remote --tags $FIRMWARE_REPO | sort -t '/' -k 3 -V | awk -F/ '{ print $3 }' | awk '!/\^\{\}/' | tail -n 1)
 
+echo "Latest version of firmware: ${latest_version}"
+
 rm -rf /tmp/firmware/
 mkdir -p /tmp/firmware
 git clone --branch $latest_version --depth 1 $FIRMWARE_REPO /tmp/firmware
 
 sudo cp -rf /tmp/firmware/* /lib/firmware
+
+
+sudo update-initramfs -c -k all
