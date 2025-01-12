2126d8e08742261f0ca7c4675da726ed3db69f0b
Igor Zarvanskyi <iclutcher@gmail.com>
2025-01-12 14:51:17 +0100

    feat(software): update insync installation

--- scripts/install_insync.sh
+++ scripts/install_insync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget -q "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xaeeb94e9c5a3b54ecfa4a66aa684470caccaf35c" -O- | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/insync.gpg
-sudo add-apt-repository "deb [arch=amd64] http://apt.insync.io/ubuntu jammy non-free contrib"
+sudo add-apt-repository "deb [arch=amd64] http://apt.insync.io/debian bookworm non-free contrib"
 
 sudo apt update
 sudo apt install insync
